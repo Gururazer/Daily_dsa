5db4992317f23524361eac285ed2fd84c1bf0b2e
GURURAZER <s3h6a9n@gmail.com>
2025-08-30 23:15:05 +0530

    update 3

---
+++ DAY-0/27. Remove Element.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int cnt = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == val) {
+                nums[i] = 101;
+                cnt++;
+            }
+        }
+        Arrays.sort(nums);
+        return nums.length - cnt;
+    }
+}
